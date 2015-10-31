0b5d505ed440ac49e5accb5b9d64453420f3082c
j16out <avrocf105@live.com>
2015-10-30 17:17:19 -0700

    compiling sh

--- comp.sh
+++ comp.sh
@@ -17,6 +17,7 @@ rm test1
 #macro2
 #g++ macro2.cpp GPT.cpp mroot.cpp -o2 -o mtest2 `root-config --cflags --glibs` -std=c++0x -pthread
 #macro1
+
 g++ macro.cpp GPT.cpp mroot.cpp -o2 -o test1 `root-config --cflags --glibs` -std=c++0x -pthread
 
 echo "done!"
